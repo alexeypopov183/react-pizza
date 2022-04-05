f5f2b5ce5b295cdcb378d7620951c19045bb2c26
Alexey Popov <alexeypopov183@gmail.com>
2022-04-05 14:39:10 +0300

    feat: add PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route, Routes} from "react-router-dom";
+import axios from "axios";
 
 import {Header} from "./components";
 import {Home, Cart} from "./pages";
 
 function App() {
-  return (<div className="wrapper">
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json')
+      .then(({data}) => setPizzas(data.pizzas));
+  }, []);
+
+  return (
+    <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home items={pizzas} />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
-    </div>);
+    </div>
+  );
 }
 
 export default App;
